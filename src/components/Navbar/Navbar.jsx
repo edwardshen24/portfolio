76d7efb3a2e57c57653185fac1d86b28272c8d31
Edward Shen <edwardshen24@gmail.com>
2022-09-19 10:13:55 -0700

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ const Navbar = () => {
   return (
     <div>
         <div class = "navbar_home">Home</div>
+        <div class = "navbar_about">About me</div>
+        <div class = "navbar_contact">Contact</div>
     </div>
   )
 }

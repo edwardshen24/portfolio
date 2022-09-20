1903c7516a5e33084aead492f1c69ee2c87c8c3d
Edward Shen <edwardshen24@gmail.com>
2022-09-20 10:31:08 -0700

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Navbar.css';
 
+// put links to stuff on the top 
 const Navbar = () => {
   return (
     <div className = "navbar">
         <div className = "navbar_home">Home</div>
+        <div className = "navbar_education">Education</div>
     </div>
   )
 }
